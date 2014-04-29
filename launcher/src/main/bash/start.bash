6eb2b35235d9c22b898aef530469c711b05c133a
shain <sha1n@users.noreply.github.com>
2014-04-29 10:23:53 +0300

    Aligned with the startup 'sh' script

--- launcher/src/main/bash/start.bash
+++ launcher/src/main/bash/start.bash
@@ -3,10 +3,16 @@
 # Uncomment and set in not set by the calling environment
 #JAVA_HOME=
 
-# Set JVM argument (addative)
-JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -XX:+PrintCommandLineFlags -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
+# JVM arguments - remote debug argument
+JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+ 
+# JVM arguments - GC options
+JAVA_OPTS="$JAVA_OPTS -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
 
-# Set Java system properties
+# JVM arguments - print startup JVM argument
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintCommandLineFlags"
+
+# JVM arguments - application configuration and jetty system properties  
 JAVA_OPTS="$JAVA_OPTS -Dconfiguration=production -Djetty.home=. -Djetty.logs=./logs"
 
 # Start Server
