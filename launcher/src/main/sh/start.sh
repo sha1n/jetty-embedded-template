07782852977c9fdf62c3518bf1b2ef0f58d05f15
shain <shai.nagar@hp.com>
2014-04-29 10:23:24 +0300

    Added 'sh' startup script

---
+++ launcher/src/main/sh/start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Uncomment and set in not set by the calling environment
+#JAVA_HOME=
+
+# JVM arguments - remote debug argument
+JAVA_OPTS="$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+
+# JVM arguments - GC options
+JAVA_OPTS="$JAVA_OPTS -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
+
+# JVM arguments - print startup JVM argument
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintCommandLineFlags"
+
+# JVM arguments - application configuration and jetty system properties
+JAVA_OPTS="$JAVA_OPTS -Dconfiguration=production -Djetty.home=. -Djetty.logs=./logs"
+
+# Start Server
+"$JAVA_HOME/bin/java" $JAVA_OPTS -jar launcher-1.0-SNAPSHOT.jar
+
+
